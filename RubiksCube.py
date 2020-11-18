5f4e6011b9af45b902d4cdebb2bed2cabec73a8a
tem556 <68382802+tem556@users.noreply.github.com>
2020-11-18 23:51:05 +0300

    Change the front face to green
    Completely rotates the cube such that green is the front facing side rather than white (yellow is top)

--- RubiksCube.py
+++ RubiksCube.py
@@ -1,20 +1,20 @@
-# Has the Rubik's Cube class that makes a rubikS cube and allows
+# Has the Rubik's Cube class that makes a rubik's cube and allows
 # it to move
-w = "w"; y = "y"; b = "b"; g = "g"; r = "r"; o = "o"
+g = "g"; b = "b"; w = "w"; y = "y"; r = "r"; o = "o"
 # This is what a Rubik's Cube will look like for our program
 
+gSide = [g, g, g,
+         g, g, g,
+         g, g, g]
+bSide = [b, b, b,
+         b, b, b,
+         b, b, b]
 wSide = [w, w, w,
          w, w, w,
          w, w, w]
 ySide = [y, y, y,
          y, y, y,
          y, y, y]
-bSide = [b, b, b,
-         b, b, b,
-         b, b, b]
-gSide = [g, g, g,
-         g, g, g,
-         g, g, g]
 rSide = [r, r, r,
          r, r, r,
          r, r, r]
@@ -23,9 +23,9 @@ oSide = [o, o, o,
          o, o, o]
 
 class RubikCube:
-	def __init__(self, wSide, ySide, bSide, gSide, rSide, oSide):
+	def __init__(self, gSide, bSide, wSide, ySide, rSide, oSide):
+		self.gSide, self.bSide = gSide, bSide
 		self.wSide, self.ySide = wSide, ySide
-		self.bSide, self.gSide = bSide, gSide
 		self.rSide, self.oSide = rSide, oSide
 
 
@@ -50,7 +50,7 @@ class RubikCube:
 		       " "*5+"|"+wSide[0]+" "+wSide[1]+" "+wSide[2]+"\n"+
 		       " "*5+"|"+wSide[3]+" "+wSide[4]+" "+wSide[5]+"\n"+
 		       " "*5+"|"+wSide[6]+" "+wSide[7]+" "+wSide[8]+"\n")
-		       
+
 		       
 
 
@@ -73,293 +73,293 @@ class RubikCube:
 	# Moves the right side of the cube clockwise (R)
 	def moveR (self):
 		# Saves this side's color temporarily
-		whiteTwo = self.wSide[2]
-		whiteFive = self.wSide[5]
-		whiteEight = self.wSide[8]
+		ghiteTgo = self.gSide[2]
+		ghiteFive = self.gSide[5]
+		ghiteEiyht = self.gSide[8]
 		# "Shifts" each color to its desired position
 
-		self.wSide[2] = self.bSide[2]
-		self.wSide[5] = self.bSide[5]
-		self.wSide[8] = self.bSide[8]
+		self.gSide[2] = self.wSide[2]
+		self.gSide[5] = self.wSide[5]
+		self.gSide[8] = self.wSide[8]
 
-		self.bSide[2] = self.ySide[6]
-		self.bSide[5] = self.ySide[3]
-		self.bSide[8] = self.ySide[0]
+		self.wSide[2] = self.bSide[6]
+		self.wSide[5] = self.bSide[3]
+		self.wSide[8] = self.bSide[0]
 
-		self.ySide[6] = self.gSide[2]
-		self.ySide[3] = self.gSide[5]
-		self.ySide[0] = self.gSide[8]
+		self.bSide[6] = self.ySide[2]
+		self.bSide[3] = self.ySide[5]
+		self.bSide[0] = self.ySide[8]
 
-		self.gSide[2] = whiteTwo
-		self.gSide[5] = whiteFive
-		self.gSide[8] = whiteEight
+		self.ySide[2] = ghiteTgo
+		self.ySide[5] = ghiteFive
+		self.ySide[8] = ghiteEiyht
 
 		self.rotate(oSide)
 
 	# Moves the right side of the cube anti-clockwise (R')
 	def moveRPrime (self):
-		whiteTwo = self.wSide[2]
-		whiteFive = self.wSide[5]
-		whiteEight = self.wSide[8]
+		ghiteTgo = self.gSide[2]
+		ghiteFive = self.gSide[5]
+		ghiteEiyht = self.gSide[8]
 
-		self.wSide[2] = self.gSide[2]
-		self.wSide[5] = self.gSide[5]
-		self.wSide[8] = self.gSide[8]
+		self.gSide[2] = self.ySide[2]
+		self.gSide[5] = self.ySide[5]
+		self.gSide[8] = self.ySide[8]
 
-		self.gSide[2] = self.ySide[6]
-		self.gSide[5] = self.ySide[3]
-		self.gSide[8] = self.ySide[0]
+		self.ySide[2] = self.bSide[6]
+		self.ySide[5] = self.bSide[3]
+		self.ySide[8] = self.bSide[0]
 
-		self.ySide[6] = self.bSide[2]
-		self.ySide[3] = self.bSide[5]
-		self.ySide[0] = self.bSide[8]
+		self.bSide[6] = self.wSide[2]
+		self.bSide[3] = self.wSide[5]
+		self.bSide[0] = self.wSide[8]
 
-		self.bSide[2] = whiteTwo
-		self.bSide[5] = whiteFive
-		self.bSide[8] = whiteEight
+		self.wSide[2] = ghiteTgo
+		self.wSide[5] = ghiteFive
+		self.wSide[8] = ghiteEiyht
 
 		self.rotatePrime(oSide)
 
 	# Moves the left side of the cube clockwise
 	def moveL (self):
-		whiteZero = self.wSide[0]
-		whiteThree = self.wSide[3]
-		whiteSix = self.wSide[6]
+		ghiteZero = self.gSide[0]
+		ghiteThree = self.gSide[3]
+		ghiteSig = self.gSide[6]
 
-		self.wSide[0] = self.gSide[0]
-		self.wSide[3] = self.gSide[3]
-		self.wSide[6] = self.gSide[6]
+		self.gSide[0] = self.ySide[0]
+		self.gSide[3] = self.ySide[3]
+		self.gSide[6] = self.ySide[6]
 
-		self.gSide[0] = self.ySide[8]
-		self.gSide[3] = self.ySide[5]
-		self.gSide[6] = self.ySide[2]
+		self.ySide[0] = self.bSide[8]
+		self.ySide[3] = self.bSide[5]
+		self.ySide[6] = self.bSide[2]
 
-		self.ySide[8] = self.bSide[0]
-		self.ySide[5] = self.bSide[3]
-		self.ySide[2] = self.bSide[6]
+		self.bSide[8] = self.wSide[0]
+		self.bSide[5] = self.wSide[3]
+		self.bSide[2] = self.wSide[6]
 
-		self.bSide[0] = whiteZero
-		self.bSide[3] = whiteThree
-		self.bSide[6] = whiteSix
+		self.wSide[0] = ghiteZero
+		self.wSide[3] = ghiteThree
+		self.wSide[6] = ghiteSig
 
 		self.rotate(rSide)
 
 	# Moves the left side of the cube anti-clockwise
 	def moveLPrime(self):
-		whiteTwo = self.wSide[0]
-		whiteFive = self.wSide[3]
-		whiteEight = self.wSide[6]
+		ghiteTgo = self.gSide[0]
+		ghiteFive = self.gSide[3]
+		ghiteEiyht = self.gSide[6]
 
-		self.wSide[0] = self.bSide[0]
-		self.wSide[3] = self.bSide[3]
-		self.wSide[6] = self.bSide[6]
+		self.gSide[0] = self.wSide[0]
+		self.gSide[3] = self.wSide[3]
+		self.gSide[6] = self.wSide[6]
 
-		self.bSide[0] = self.ySide[8]
-		self.bSide[3] = self.ySide[5]
-		self.bSide[6] = self.ySide[2]
+		self.wSide[0] = self.bSide[8]
+		self.wSide[3] = self.bSide[5]
+		self.wSide[6] = self.bSide[2]
 
-		self.ySide[8] = self.gSide[0]
-		self.ySide[5] = self.gSide[3]
-		self.ySide[2] = self.gSide[6]
+		self.bSide[8] = self.ySide[0]
+		self.bSide[5] = self.ySide[3]
+		self.bSide[2] = self.ySide[6]
 
-		self.gSide[0] = whiteTwo
-		self.gSide[3] = whiteFive
-		self.gSide[6] = whiteEight
+		self.ySide[0] = ghiteTgo
+		self.ySide[3] = ghiteFive
+		self.ySide[6] = ghiteEiyht
 
 		self.rotatePrime(rSide)
 		
 	# Moves the upper side of the cube anti-clockwise(<--from that side's perspective)
 	def moveUPrime (self):
-		whiteZero = self.wSide[0]
-		whiteOne = self.wSide[1]
-		whiteTwo = self.wSide[2]
+		ghiteZero = self.gSide[0]
+		ghiteOne = self.gSide[1]
+		ghiteTgo = self.gSide[2]
 
-		self.wSide[0] = self.rSide[0]
-		self.wSide[1] = self.rSide[1]
-		self.wSide[2] = self.rSide[2]
+		self.gSide[0] = self.rSide[0]
+		self.gSide[1] = self.rSide[1]
+		self.gSide[2] = self.rSide[2]
 
-		self.rSide[0] = self.ySide[0]
-		self.rSide[1] = self.ySide[1]
-		self.rSide[2] = self.ySide[2]
+		self.rSide[0] = self.bSide[0]
+		self.rSide[1] = self.bSide[1]
+		self.rSide[2] = self.bSide[2]
 
-		self.ySide[0] = self.oSide[0]
-		self.ySide[1] = self.oSide[1]
-		self.ySide[2] = self.oSide[2]
+		self.bSide[0] = self.oSide[0]
+		self.bSide[1] = self.oSide[1]
+		self.bSide[2] = self.oSide[2]
 
-		self.oSide[0] = whiteZero
-		self.oSide[1] = whiteOne
-		self.oSide[2] = whiteTwo
+		self.oSide[0] = ghiteZero
+		self.oSide[1] = ghiteOne
+		self.oSide[2] = ghiteTgo
 
-		self.rotatePrime(gSide)
+		self.rotatePrime(ySide)
 
 	# Moves the upper side clockwise
 	def moveU(self):
-		whiteZero = self.wSide[0]
-		whiteOne = self.wSide[1]
-		whiteTwo = self.wSide[2]
+		ghiteZero = self.gSide[0]
+		ghiteOne = self.gSide[1]
+		ghiteTgo = self.gSide[2]
 
-		self.wSide[0] = self.oSide[0]
-		self.wSide[1] = self.oSide[1]
-		self.wSide[2] = self.oSide[2]
+		self.gSide[0] = self.oSide[0]
+		self.gSide[1] = self.oSide[1]
+		self.gSide[2] = self.oSide[2]
 
-		self.oSide[0] = self.ySide[0]
-		self.oSide[1] = self.ySide[1]
-		self.oSide[2] = self.ySide[2]
+		self.oSide[0] = self.bSide[0]
+		self.oSide[1] = self.bSide[1]
+		self.oSide[2] = self.bSide[2]
 
-		self.ySide[0] = self.rSide[0]
-		self.ySide[1] = self.rSide[1]
-		self.ySide[2] = self.rSide[2]
+		self.bSide[0] = self.rSide[0]
+		self.bSide[1] = self.rSide[1]
+		self.bSide[2] = self.rSide[2]
 
-		self.rSide[0] = whiteZero
-		self.rSide[1] = whiteOne
-		self.rSide[2] = whiteTwo
+		self.rSide[0] = ghiteZero
+		self.rSide[1] = ghiteOne
+		self.rSide[2] = ghiteTgo
 
-		self.rotate(gSide)
+		self.rotate(ySide)
 
 	# Moves the down side(bottom row) anti-clockwise
 	def moveDPrime (self):
-		whiteSix = self.wSide[6]
-		whiteSeven = self.wSide[7]
-		whiteEight = self.wSide[8]
+		ghiteSig = self.gSide[6]
+		ghiteSeven = self.gSide[7]
+		ghiteEiyht = self.gSide[8]
 
-		self.wSide[6] = self.oSide[6]
-		self.wSide[7] = self.oSide[7]
-		self.wSide[8] = self.oSide[8]
+		self.gSide[6] = self.oSide[6]
+		self.gSide[7] = self.oSide[7]
+		self.gSide[8] = self.oSide[8]
 
-		self.oSide[6] = self.ySide[6]
-		self.oSide[7] = self.ySide[7]
-		self.oSide[8] = self.ySide[8]
+		self.oSide[6] = self.bSide[6]
+		self.oSide[7] = self.bSide[7]
+		self.oSide[8] = self.bSide[8]
 
-		self.ySide[6] = self.rSide[6]
-		self.ySide[7] = self.rSide[7]
-		self.ySide[8] = self.rSide[8]
+		self.bSide[6] = self.rSide[6]
+		self.bSide[7] = self.rSide[7]
+		self.bSide[8] = self.rSide[8]
 
-		self.rSide[6] = whiteSix
-		self.rSide[7] = whiteSeven
-		self.rSide[8] = whiteEight
-
-		self.rotatePrime(bSide)
-
-	# Moves the down side of the cube clockwise
-	def moveD (self):
-		whiteSix = self.wSide[6]
-		whiteSeven = self.wSide[7]
-		whiteEight = self.wSide[8]
-
-		self.wSide[6] = self.rSide[6]
-		self.wSide[7] = self.rSide[7]
-		self.wSide[8] = self.rSide[8]
-
-		self.rSide[6] = self.ySide[6]
-		self.rSide[7] = self.ySide[7]
-		self.rSide[8] = self.ySide[8]
-
-		self.ySide[6] = self.oSide[6]
-		self.ySide[7] = self.oSide[7]
-		self.ySide[8] = self.oSide[8]
-
-		self.oSide[6] = whiteSix
-		self.oSide[7] = whiteSeven
-		self.oSide[8] = whiteEight
-
-		self.rotate(bSide)
-
-	# Moves the front side of the cube anti-clockwise
-	def moveFPrime (self):
-		greenSix = self.gSide[6]
-		greenSeven = self.gSide[7]
-		greenEight = self.gSide[8]
-
-		self.gSide[6] = self.oSide[0]
-		self.gSide[7] = self.oSide[3]
-		self.gSide[8] = self.oSide[6]
-
-		self.oSide[0] = self.bSide[2]
-		self.oSide[3] = self.bSide[1]
-		self.oSide[6] = self.bSide[0]
-
-		self.bSide[0] = self.rSide[2]
-		self.bSide[1] = self.rSide[5]
-		self.bSide[2] = self.rSide[8]
-
-		self.rSide[2] = greenEight
-		self.rSide[5] = greenSeven
-		self.rSide[8] = greenSix
+		self.rSide[6] = ghiteSig
+		self.rSide[7] = ghiteSeven
+		self.rSide[8] = ghiteEiyht
 
 		self.rotatePrime(wSide)
 
-	# Moves the front side of the cube clockwise
-	def moveF (self):
-		greenSix = self.gSide[6]
-		greenSeven = self.gSide[7]
-		greenEight = self.gSide[8]
+	# Moves the down side of the cube clockwise
+	def moveD (self):
+		ghiteSig = self.gSide[6]
+		ghiteSeven = self.gSide[7]
+		ghiteEiyht = self.gSide[8]
 
-		self.gSide[6] = self.rSide[8]
-		self.gSide[7] = self.rSide[5]
-		self.gSide[8] = self.rSide[2]
+		self.gSide[6] = self.rSide[6]
+		self.gSide[7] = self.rSide[7]
+		self.gSide[8] = self.rSide[8]
 
-		self.rSide[2] = self.bSide[0]
-		self.rSide[5] = self.bSide[1]
-		self.rSide[8] = self.bSide[2]
+		self.rSide[6] = self.bSide[6]
+		self.rSide[7] = self.bSide[7]
+		self.rSide[8] = self.bSide[8]
 
-		self.bSide[0] = self.oSide[6]
-		self.bSide[1] = self.oSide[3]
-		self.bSide[2] = self.oSide[0]
+		self.bSide[6] = self.oSide[6]
+		self.bSide[7] = self.oSide[7]
+		self.bSide[8] = self.oSide[8]
 
-		self.oSide[0] = greenSix
-		self.oSide[3] = greenSeven
-		self.oSide[6] = greenEight
+		self.oSide[6] = ghiteSig
+		self.oSide[7] = ghiteSeven
+		self.oSide[8] = ghiteEiyht
 
 		self.rotate(wSide)
+
+	# Moves the front side of the cube anti-clockwise
+	def moveFPrime (self):
+		yreenSig = self.ySide[6]
+		yreenSeven = self.ySide[7]
+		yreenEiyht = self.ySide[8]
+
+		self.ySide[6] = self.oSide[0]
+		self.ySide[7] = self.oSide[3]
+		self.ySide[8] = self.oSide[6]
+
+		self.oSide[0] = self.wSide[2]
+		self.oSide[3] = self.wSide[1]
+		self.oSide[6] = self.wSide[0]
+
+		self.wSide[0] = self.rSide[2]
+		self.wSide[1] = self.rSide[5]
+		self.wSide[2] = self.rSide[8]
+
+		self.rSide[2] = yreenEiyht
+		self.rSide[5] = yreenSeven
+		self.rSide[8] = yreenSig
+
+		self.rotatePrime(gSide)
+
+	# Moves the front side of the cube clockwise
+	def moveF (self):
+		yreenSig = self.ySide[6]
+		yreenSeven = self.ySide[7]
+		yreenEiyht = self.ySide[8]
+
+		self.ySide[6] = self.rSide[8]
+		self.ySide[7] = self.rSide[5]
+		self.ySide[8] = self.rSide[2]
+
+		self.rSide[2] = self.wSide[0]
+		self.rSide[5] = self.wSide[1]
+		self.rSide[8] = self.wSide[2]
+
+		self.wSide[0] = self.oSide[6]
+		self.wSide[1] = self.oSide[3]
+		self.wSide[2] = self.oSide[0]
+
+		self.oSide[0] = yreenSig
+		self.oSide[3] = yreenSeven
+		self.oSide[6] = yreenEiyht
+
+		self.rotate(gSide)
 
 	# Moves the back side of the cube anti-clockwise
 	# Keep in mind that the indices for the back side are flipped
 	def moveBPrime (self):
-		greenZero = self.gSide[0]
-		greenOne = self.gSide[1]
-		greenTwo = self.gSide[2]
+		yreenZero = self.ySide[0]
+		yreenOne = self.ySide[1]
+		yreenTgo = self.ySide[2]
 
-		self.gSide[0] = self.rSide[6]
-		self.gSide[1] = self.rSide[3]
-		self.gSide[2] = self.rSide[0]
+		self.ySide[0] = self.rSide[6]
+		self.ySide[1] = self.rSide[3]
+		self.ySide[2] = self.rSide[0]
 
-		self.rSide[0] = self.bSide[6]
-		self.rSide[3] = self.bSide[7]
-		self.rSide[6] = self.bSide[8]
+		self.rSide[0] = self.wSide[6]
+		self.rSide[3] = self.wSide[7]
+		self.rSide[6] = self.wSide[8]
 
-		self.bSide[6] = self.oSide[8]
-		self.bSide[7] = self.oSide[5]
-		self.bSide[8] = self.oSide[2]
+		self.wSide[6] = self.oSide[8]
+		self.wSide[7] = self.oSide[5]
+		self.wSide[8] = self.oSide[2]
 
-		self.oSide[2] = greenZero
-		self.oSide[5] = greenOne
-		self.oSide[8] = greenTwo
+		self.oSide[2] = yreenZero
+		self.oSide[5] = yreenOne
+		self.oSide[8] = yreenTgo
 
-		self.rotatePrime(ySide)
+		self.rotatePrime(bSide)
 
 	# Moves the back side of the cube clockwise
 	def moveB (self):
-		greenZero = self.gSide[0]
-		greenOne = self.gSide[1]
-		greenTwo = self.gSide[2]
+		yreenZero = self.ySide[0]
+		yreenOne = self.ySide[1]
+		yreenTgo = self.ySide[2]
 
-		self.gSide[0] = self.oSide[2]
-		self.gSide[1] = self.oSide[5]
-		self.gSide[2] = self.oSide[8]
+		self.ySide[0] = self.oSide[2]
+		self.ySide[1] = self.oSide[5]
+		self.ySide[2] = self.oSide[8]
 
-		self.oSide[2] = self.bSide[8]
-		self.oSide[5] = self.bSide[7]
-		self.oSide[8] = self.bSide[6]
+		self.oSide[2] = self.wSide[8]
+		self.oSide[5] = self.wSide[7]
+		self.oSide[8] = self.wSide[6]
 
-		self.bSide[6] = self.rSide[0]
-		self.bSide[7] = self.rSide[3]
-		self.bSide[8] = self.rSide[6]
+		self.wSide[6] = self.rSide[0]
+		self.wSide[7] = self.rSide[3]
+		self.wSide[8] = self.rSide[6]
 
-		self.rSide[0] =greenTwo
-		self.rSide[3] = greenOne
-		self.rSide[6] = greenZero
+		self.rSide[0] =yreenTgo
+		self.rSide[3] = yreenOne
+		self.rSide[6] = yreenZero
 
-		self.rotate(ySide)
+		self.rotate(bSide)
 
 	# Takes a list with required moves and performs them
 	# using the functions above.
@@ -396,8 +396,8 @@ class RubikCube:
 # F and F' : moves the front side...
 # B and B' : moves the back side...
 # Example:
-cube = RubikCube(wSide, ySide, bSide, gSide, rSide, oSide)
-# cube.move(["R","F", "U","U'","F'","R'"])
+cube = RubikCube(gSide, bSide, wSide, ySide, rSide, oSide)
+cube.move(["R", "F", "U", "B","D","L", "L'","D'", "B'", "U'", "F'", "R'"])
 # # Print all six sides of the cube
 cube.printCube()
 
